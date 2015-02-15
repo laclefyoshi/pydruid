e11466c6642d5a36100d1a6064623d5c6ac466a0
SAEKI Yoshiyasu <laclef.yoshiyasu@gmail.com>
2015-02-15 14:03:12 +0900

    add filtered aggregator, add HyperUniqueCardinality post-aggregator

--- pydruid/utils/aggregators.py
+++ pydruid/utils/aggregators.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+from filters import Filter
 
 def longsum(raw_metric):
     return {"type": "longSum", "fieldName": raw_metric}
@@ -34,8 +35,10 @@ def max(raw_metric):
 def count():
     return {"type": "count"}
 
+
 def hyperunique(raw_metric):
     return {"type": "hyperUnique", "fieldName": raw_metric}
+
 
 def javascript(field_names, fn_aggregate, fn_combine, fn_reset):
     return {"type": "javascript",
@@ -44,10 +47,18 @@ def javascript(field_names, fn_aggregate, fn_combine, fn_reset):
             "fnCombine": fn_combine,
             "fnReset": fn_reset}
 
-def cardinality(field_names, byRow=False):
+
+def cardinality(field_names, by_row=False):
     return {"type": "cardinality",
             "fieldNames": field_names,
-            "byRow": byRow}
+            "byRow": by_row}
+
+
+def filtered(filter, aggregator):
+    return {"type": "filtered",
+            "filter": Filter.build_filter(filter),
+            "aggregator": build_aggregators(aggregator)[0]}
+
 
 def build_aggregators(agg_input):
     return [dict([('name', k)] + v.items())
